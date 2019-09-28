ee81baedd91203790188813b85b64122a2b940ed
Luiz F. Pinheiro <luizfelipenet@hotmail.com>
2019-09-28 17:37:38 -0300

    Update calendar info

--- rhema_web/src/components/CalendarModal.vue
+++ rhema_web/src/components/CalendarModal.vue
@@ -1,16 +1,28 @@
 <template>
   <v-row justify="center">
     <v-btn bottom text @click="dialog = true ? !dialog : true">AGENDA</v-btn>
+    <v-dialog v-model="dialog" transition="dialog-bottom-transition" width="400">
+      <v-card hover>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">Culto de Celebração</v-list-item-title>
+            <v-list-item-subtitle class="subtitle-1 pa-2">Domingos - 19H</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition" max-width="290">
-      <v-card>
-        <v-card-title class="headline">Use Google's location service?</v-card-title>
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title class="title">Redes</v-list-item-title>
+            <v-list-item-subtitle class="subtitle-1 pa-2">Sábados - 19:30H</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-        <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-
-        <v-card-actions>
-          <div class="flex-grow-1"></div>
-        </v-card-actions>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="title">Escola Bíblica Discipular</v-list-item-title>
+            <v-list-item-subtitle class="subtitle-1 pa-2">Quintas-Feiras - 20H</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </v-card>
     </v-dialog>
   </v-row>
@@ -19,7 +31,22 @@
 <script>
 export default {
   data: () => ({
-    "dialog": false
-  }),
+    dialog: false,
+    items: [
+      {
+        icon: "mdi-home",
+        text: "DOMINGO - 19H"
+      },
+      {
+        icon: "mdi-home",
+        text: "SÁBADO - 19:30H"
+      },
+      {
+        icon: "mdi-home",
+        text: "QUINTA-FEIRA - 20H"
+      }
+    ],
+    model: 1
+  })
 };
 </script>
